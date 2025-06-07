b508185cd11112141534b1911c4fbd087d4df96a
nk <klimov@mailbox.org>
2025-06-07 22:21:16 +0200

    Add project files.

---
+++ scripts/copy-debug-dll-to-output.bat
@@ -0,0 +1,5 @@
+xcopy %~dp0..\SFML\bin\sfml-audio-d-3.dll ^
+      %~dp0..\SFML\bin\sfml-graphics-d-3.dll ^
+      %~dp0..\SFML\bin\sfml-system-d-3.dll  ^
+      %~dp0..\SFML\bin\sfml-window-d-3.dll ^
+%~dp0..\bin\Debug  /E /D /Y
