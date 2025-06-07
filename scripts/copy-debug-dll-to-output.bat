29eaddd3bc52babac04c3c846dcb32b0b43ec1fa
nk <klimov@mailbox.org>
2025-06-07 22:33:16 +0200

    ----

--- scripts/copy-debug-dll-to-output.bat
+++
@@ -1,5 +0,0 @@
-xcopy %~dp0..\SFML\bin\sfml-audio-d-3.dll ^
-      %~dp0..\SFML\bin\sfml-graphics-d-3.dll ^
-      %~dp0..\SFML\bin\sfml-system-d-3.dll  ^
-      %~dp0..\SFML\bin\sfml-window-d-3.dll ^
-%~dp0..\bin\Debug  /E /D /Y
